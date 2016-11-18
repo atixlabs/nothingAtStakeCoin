084ae13e0b71c4dfa267c1a88ae781e67fd269d2
ntallar <ntallar@atixlabs.com>
2016-11-18 12:54:35 -0300

    bestNChains implementation changed

---
+++ src/test/scala/scorex/nothingAtStakeCoin/NothingAtStakeCoinHistorySpec.scala
@@ -0,0 +1,13 @@
+package scorex.nothingAtStakeCoin.history
+
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.{Matchers, PropSpec}
+import scorex.nothingAtStakeCoin.ObjectGenerators
+
+class NothingAtStakeCoinNodeNodeHistorySpec extends PropSpec
+  with PropertyChecks
+  with GeneratorDrivenPropertyChecks
+  with Matchers
+  with ObjectGenerators {
+
+}
