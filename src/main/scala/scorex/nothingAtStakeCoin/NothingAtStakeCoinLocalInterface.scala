b90ddc1b017138c5bca1c3748d5ec33aa87a6be1
Alan Verbner <averbner@atixlabs.com>
2016-11-30 18:11:52 -0300

    Fixme removal

--- src/main/scala/scorex/nothingAtStakeCoin/NothingAtStakeCoinLocalInterface.scala
+++ src/main/scala/scorex/nothingAtStakeCoin/NothingAtStakeCoinLocalInterface.scala
@@ -9,31 +9,17 @@ import scorex.nothingAtStakeCoin.transaction.NothingAtStakeCoinTransaction
 class NothingAtStakeCoinLocalInterface(override val viewHolderRef: ActorRef)
   extends LocalInterface[PublicKey25519Proposition, NothingAtStakeCoinTransaction, NothingAtStakeCoinBlock] {
 
-  override protected def onStartingPersistentModifierApplication(pmod: NothingAtStakeCoinBlock): Unit = {
-    // TODO
-  }
+  override protected def onStartingPersistentModifierApplication(pmod: NothingAtStakeCoinBlock): Unit = Unit
 
-  override protected def onFailedTransaction(tx: NothingAtStakeCoinTransaction): Unit = {
-    // TODO
-  }
+  override protected def onFailedTransaction(tx: NothingAtStakeCoinTransaction): Unit = Unit
 
-  override protected def onFailedModification(mod: NothingAtStakeCoinBlock): Unit = {
-    // TODO
-  }
+  override protected def onFailedModification(mod: NothingAtStakeCoinBlock): Unit = Unit
 
-  override protected def onSuccessfulTransaction(tx: NothingAtStakeCoinTransaction): Unit = {
-    // TODO
-  }
+  override protected def onSuccessfulTransaction(tx: NothingAtStakeCoinTransaction): Unit = Unit
 
-  override protected def onSuccessfulModification(mod: NothingAtStakeCoinBlock): Unit = {
-    // TODO
-  }
+  override protected def onSuccessfulModification(mod: NothingAtStakeCoinBlock): Unit = Unit
 
-  override protected def onNoBetterNeighbour(): Unit = {
-    // TODO
-  }
+  override protected def onNoBetterNeighbour(): Unit = Unit
 
-  override protected def onBetterNeighbourAppeared(): Unit = {
-    // TODO
-  }
+  override protected def onBetterNeighbourAppeared(): Unit = Unit
 }
